e41d377edc32394c72bbbea9b711fc554c7cc3e0
lduer <duerlukas@gmail.com>
2015-08-20 18:32:09 +0200

    added files, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # js-svg-fallback
-small js libray to fix the svg-display in MS Internet Explorer
+
+This is a small js libray to fix the svg-display in MS Internet Explorer.
+
+## Installation
+
+### Via Gihub-CDN: 
+
+Simply add the JS-File to your Page (the CSS-File is added by default)
+
+```html
+<script type="text/javascript" src="https://lduer.github.io/js-svg-fallback/src/svg-fallback.js" id="js-fallback-script"></script>
+```
+
+> **Note**: If you don't have jQuery enabled, the Script must be added just before the ``</body>``-Tag to work.
+
+You may have noticed the ``id``-attribute: If you don't want to configure the functions inside the ``#js-fallback-script`` is used to identify the JS source-file and load the CSS file with the additions.
+
+### Bower-installation
+
+Add the following lines to your bower-configuration ``bower.json`
+
+```
+{
+    ...
+    "dependencies": {
+        ...
+        "js-svg-fallback": "https://github.com/lduer/js-svg-fallback.git"
+        ...
+}
+
+
+```
+
+### manual Download
+
+Download the whole Repository from [GitHub](https://github.com/lduer/js-svg-fallback/archive/master.zip)
+
+
+## Configuration
+
+TODO!!
---
+++ src/svg-fallback.js
@@ -0,0 +1,76 @@
+/**
+ * Created by lduer on 20.08.15.
+ */
+
+// detect if the current browser is IE or "MS Edge"
+function isIE(userAgent) {
+    userAgent = userAgent || navigator.userAgent;
+
+    var html = document.getElementsByTagName('html')[0];
+    var state = document.readyState
+    if (['interactive', 'complete'].indexOf(document.readyState) > -1 && (html.classList.contains('is-ie') || html.classList.contains('not-ie'))) {
+        return html.classList.contains('is-ie');
+    } else {
+        var isIE = userAgent.indexOf("MSIE ") > -1 || userAgent.indexOf("Trident/") > -1 || userAgent.indexOf("Edge/") > -1;
+        if (isIE) {
+            html.classList.add('is-ie');
+        } else {
+            html.classList.add('not-ie');
+        }
+        return isIE;
+    }
+}
+
+// add the stylesheet
+function loadIEfallbackStyle(stylesheetUrl){
+    var stylesheetUrl = stylesheetUrl||document.getElementById('js-fallback-script').src.replace('.js', '.css');
+    var linkElem = document.createElement('link');
+
+    linkElem.type = 'text/css';
+    linkElem.rel = 'stylesheet';
+    linkElem.href = stylesheetUrl;
+
+    //document.head.appendChild(linkElem);
+    document.head.insertBefore(linkElem, document.head.firstChild);
+}
+
+// call this after DOM is ready or just before the </body>-Tag
+function modifySvgImage(className) {
+    var className = className||'svg';
+
+    var svgs = document.querySelectorAll('img[src*=".svg"]'), i;
+    var replaceExtension = ((typeof Modernizr !== 'undefined') && !Modernizr.svg);
+
+    if (svgs.length === 0 || (false === isIE() && false === replaceExtension)) {
+        // break, if no svg image found
+        return;
+    }
+
+    for (i = 0; i < svgs.length; ++i) {
+        if (isIE()) {
+            svgs[i].className += ' ' + className;
+        }
+        if (replaceExtension) {
+            // browser is no capable with svg-images: replace them with .png images.
+            svgs[i].src = svgs[i].src.replace('.svg', '.png');
+        }
+    }
+}
+
+
+if (isIE() && window.jQuery) {
+    $(function(){
+        // call function after dom is ready
+        modifySvgImage('svg');
+        loadIEfallbackStyle()
+    });
+} else if (isIE()) {
+    //call addClassName() right before </body>
+    var node = document.createTextNode('modifySvgImage("svg")');
+    var scriptElem = document.createElement('script');
+    scriptElem.type = 'text/javascript';
+    scriptElem.appendChild(node);
+    document.body.appendChild(scriptElem);
+
+    loadIEfallbackStyle();
+}
